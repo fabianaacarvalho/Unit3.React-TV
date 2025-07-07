f1f67c306f4a2db1a327257fbeaf3847d42d936d
Fabiana Carvalho <fabiana.acarvalho@outlook.com>
2025-07-07 20:08:36 -0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react";
+import { tvShows } from "./shows/data";
+import ShowSelection from "./shows/ShowSelection";
+import ShowDetails from "./shows/ShowDetails";
+
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
 export default function App() {
+  const [shows] = useState(tvShows);
+  const [selectedShow, setSelectedShow] = useState();
+
   return (
     <>
       <header>
         <p>React TV</p>
       </header>
-      <main></main>
+      <main>
+        <ShowSelection
+          shows={shows}
+          selectedShow={selectedShow}
+          setSelectedShow={setSelectedShow}
+        />
+        <ShowDetails key={selectedShow?.name} show={selectedShow} />
+      </main>
     </>
   );
 }
